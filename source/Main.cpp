f8122f2dad0d1e9d72358fbeb0ca24d9feb51c4c
Benjamin Dehli <superelg@gmail.com>
2026-07-14 20:28:08 +0200

    Bump version to 1.0.0

--- source/Main.cpp
+++ source/Main.cpp
@@ -11,7 +11,7 @@ class StudioApplication : public juce::JUCEApplication
 {
 public:
     const juce::String getApplicationName() override    { return "DMSE Studio"; }
-    const juce::String getApplicationVersion() override { return "0.1.0"; }
+    const juce::String getApplicationVersion() override { return "1.0.0"; }
     bool moreThanOneInstanceAllowed() override          { return true; }
 
     void initialise (const juce::String&) override
